b77f9887d5ec2a3c5256f5c4ef5f37800d9fe2b5
Dokkae6949 <finnliry@gmail.com>
2023-01-05 20:52:26 +0100

    Scanner

--- src/main/java/Yuna/Enum/Token.java
+++
@@ -1,5 +0,0 @@
-package Yuna.Enum;
-
-public enum Token {
-
-}
--- src/main/java/Yuna/Yuna.java
+++ src/main/java/Yuna/Yuna.java
@@ -1,5 +1,69 @@
 package Yuna;
 
-public class Yuna {
+import Yuna.Token.TokenType;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class Yuna {
+    private static boolean hadError = false;
+
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+
+    public static void error(int line, String message) {
+        report(line, message);
+    }
+
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(69);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.println("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<TokenType> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        for (TokenType token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    private static void report(int line, String message) {
+        System.err.println("ERROR on LINE " + line + " > " + message);
+        hadError = true;
+    }
 }
